4784785e801a3419adac6442f72c52ea5b05c377
Trejon Winter <trejonw@gmail.com>
2020-07-24 13:01:00 -0400

    Complete integration of Yelp API

--- ravenous/src/util/Yelp.js
+++ ravenous/src/util/Yelp.js
@@ -1,3 +1,31 @@
-const clientId = 'FQALG8vSqfx0xfCGvuXWnw';
 const apiKey = 'vCjD-ED6jQeBJn2bJt_M9i2b3JoVljLvbLQmOsPan1yJwv40u2xxt6_l6jgnK3UY04yqZPkoeOQz-AtWWTJtmvQiRv7ctkMsXVfrpPB_l7spY6Pa_iu3zVr619QaX3Yx';
-const url = 'https://api.yelp.com/v3';
+
+const Yelp = {
+    search(term, location, sortBy) {
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+        headers: {
+          Authorization: `Bearer ${apiKey}`
+        }
+      }).then(response => {
+        return response.json();
+      }).then(jsonResponse => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map(business => ({
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }));
+        }
+      });
+    }
+  };
+  
+  export default Yelp;
+  
