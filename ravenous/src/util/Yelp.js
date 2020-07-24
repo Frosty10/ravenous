9856e390563e54314efd1a35d7c6d65b0dde0e7b
Trejon Winter <trejonw@gmail.com>
2020-07-24 08:45:48 -0400

    Add polyfill for fetch() api

---
+++ ravenous/src/util/Yelp.js
@@ -0,0 +1,3 @@
+const clientId = 'FQALG8vSqfx0xfCGvuXWnw';
+const apiKey = 'vCjD-ED6jQeBJn2bJt_M9i2b3JoVljLvbLQmOsPan1yJwv40u2xxt6_l6jgnK3UY04yqZPkoeOQz-AtWWTJtmvQiRv7ctkMsXVfrpPB_l7spY6Pa_iu3zVr619QaX3Yx';
+const url = 'https://api.yelp.com/v3';
